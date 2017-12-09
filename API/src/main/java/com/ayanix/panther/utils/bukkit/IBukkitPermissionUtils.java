eb43bff22c637b37f6966d3f53dd7d4a607ff9e7
Lewes D. B <lewes@ayanix.com>
2017-12-09 13:56:26 +0000

    Permission Utils
    Registering permissions.

---
+++ API/src/main/java/com/ayanix/panther/utils/bukkit/IBukkitPermissionUtils.java
@@ -0,0 +1,63 @@
+/*
+ *                                                            _...---.._
+ *                                                        _.'`       -_ ``.
+ *                                                    .-'`                 `.
+ *                                                 .-`                     q ;
+ *                                              _-`                       __  \
+ *                                          .-'`                  . ' .   \ `;/
+ *                                      _.-`                    /.      `._`/
+ *                              _...--'`                        \_`..._
+ *                           .'`                         -         `'--:._
+ *                        .-`                           \                  `-.
+ *                       .                               `-..__.....----...., `.
+ *                      '                   `'''---..-''`'              : :  : :
+ *                    .` -                '``                           `'   `'
+ *                 .-` .` '             .``
+ *             _.-` .-`   '            .
+ *         _.-` _.-`    .' '         .`
+ * (`''--'' _.-`      .'  '        .'
+ *  `'----''        .'  .`       .`
+ *                .'  .'     .-'`    _____               _    _
+ *              .'   :    .-`       |  __ \             | |  | |
+ *              `. .`   ,`          | |__) |__ _  _ __  | |_ | |__    ___  _ __
+ *               .'   .'            |  ___// _` || '_ \ | __|| '_ \  / _ \| '__|
+ *              '   .`              | |   | (_| || | | || |_ | | | ||  __/| |
+ *             '  .`                |_|    \__,_||_| |_| \__||_| |_| \___||_|
+ *             `  '.
+ *             `.___;
+ */
+package com.ayanix.panther.utils.bukkit;
+
+import org.bukkit.permissions.PermissionDefault;
+
+/**
+ * Panther - Developed by Lewes D. B.
+ * All rights reserved 2017.
+ */
+public interface IBukkitPermissionUtils
+{
+
+	/**
+	 * Register a permission, even if it already exists.
+	 * By default, OPs have this permission.
+	 *
+	 * If the permission already exists, it will be removed and re-registered.
+	 *
+	 * @param permission The permission to register.
+	 */
+	default void registerPermission(String permission)
+	{
+		registerPermission(permission, PermissionDefault.OP);
+	}
+
+	/**
+	 * Register a permission, even if it already exists, with a specific default value.
+	 *
+	 * If the permission already exists, it will be removed and re-registered.
+	 *
+	 * @param permission   The permission to register.
+	 * @param defaultValue The default value of the permission.
+	 */
+	void registerPermission(String permission, PermissionDefault defaultValue);
+
+}
---
+++ Bukkit/src/main/java/com/ayanix/panther/impl/bukkit/utils/BukkitPermissionUtils.java
@@ -0,0 +1,61 @@
+/*
+ *                                                            _...---.._
+ *                                                        _.'`       -_ ``.
+ *                                                    .-'`                 `.
+ *                                                 .-`                     q ;
+ *                                              _-`                       __  \
+ *                                          .-'`                  . ' .   \ `;/
+ *                                      _.-`                    /.      `._`/
+ *                              _...--'`                        \_`..._
+ *                           .'`                         -         `'--:._
+ *                        .-`                           \                  `-.
+ *                       .                               `-..__.....----...., `.
+ *                      '                   `'''---..-''`'              : :  : :
+ *                    .` -                '``                           `'   `'
+ *                 .-` .` '             .``
+ *             _.-` .-`   '            .
+ *         _.-` _.-`    .' '         .`
+ * (`''--'' _.-`      .'  '        .'
+ *  `'----''        .'  .`       .`
+ *                .'  .'     .-'`    _____               _    _
+ *              .'   :    .-`       |  __ \             | |  | |
+ *              `. .`   ,`          | |__) |__ _  _ __  | |_ | |__    ___  _ __
+ *               .'   .'            |  ___// _` || '_ \ | __|| '_ \  / _ \| '__|
+ *              '   .`              | |   | (_| || | | || |_ | | | ||  __/| |
+ *             '  .`                |_|    \__,_||_| |_| \__||_| |_| \___||_|
+ *             `  '.
+ *             `.___;
+ */
+package com.ayanix.panther.impl.bukkit.utils;
+
+import com.ayanix.panther.utils.bukkit.IBukkitPermissionUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+
+/**
+ * Panther - Developed by Lewes D. B.
+ * All rights reserved 2017.
+ */
+public class BukkitPermissionUtils implements IBukkitPermissionUtils
+{
+
+	@Override
+	public void registerPermission(String permission, PermissionDefault defaultValue)
+	{
+		if (permission == null)
+		{
+			return;
+		}
+
+		if (Bukkit.getPluginManager().getPermission(permission) != null)
+		{
+			Bukkit.getPluginManager().removePermission(permission);
+		}
+
+		Permission bukkitPermission = new Permission(permission, defaultValue);
+
+		Bukkit.getPluginManager().addPermission(bukkitPermission);
+	}
+
+}
