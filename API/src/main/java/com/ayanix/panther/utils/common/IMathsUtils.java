0649c76f9c2ce9243597147f73616c4757f8eb8e
Lewes D. B <lewes@ayanix.com>
2017-12-30 14:22:23 +0000

    MathsUtils

---
+++ API/src/main/java/com/ayanix/panther/utils/common/IMathsUtils.java
@@ -0,0 +1,57 @@
+/*
+ *                                                            _...---.._
+ *                                                        _.'`       -_ ``.
+ *                                                    .-'`                 `.
+ *                                                 .-`                     q ;
+ *                                              _-`                       __  \
+ *                                          .-'`                  . ' .   \ `;/
+ *                                      _.-`                    /.      `._`/
+ *                              _...--'`                        \_`..._
+ *                           .'`                         -         `'--:._
+ *                        .-`                           \                  `-.
+ *                       .                               `-..__.....----...., `.
+ *                      '                   `'''---..-''`'              : :  : :
+ *                    .` -                '``                           `'   `'
+ *                 .-` .` '             .``
+ *             _.-` .-`   '            .
+ *         _.-` _.-`    .' '         .`
+ * (`''--'' _.-`      .'  '        .'
+ *  `'----''        .'  .`       .`
+ *                .'  .'     .-'`    _____               _    _
+ *              .'   :    .-`       |  __ \             | |  | |
+ *              `. .`   ,`          | |__) |__ _  _ __  | |_ | |__    ___  _ __
+ *               .'   .'            |  ___// _` || '_ \ | __|| '_ \  / _ \| '__|
+ *              '   .`              | |   | (_| || | | || |_ | | | ||  __/| |
+ *             '  .`                |_|    \__,_||_| |_| \__||_| |_| \___||_|
+ *             `  '.
+ *             `.___;
+ */
+package com.ayanix.panther.utils.common;
+
+/**
+ * Panther - Developed by Lewes D. B.
+ * All rights reserved 2017.
+ */
+public interface IMathsUtils
+{
+
+	/**
+	 * Formats a value into readable format with commas.
+	 *
+	 * @param value The value to format.
+	 * @return Formatted number.
+	 */
+	default String format(int value)
+	{
+		return format((double) value);
+	}
+
+	/**
+	 * Formats a value into readable format with commas.
+	 *
+	 * @param value The value to format.
+	 * @return Formatted number.
+	 */
+	String format(double value);
+
+}
---
+++ Common/src/main/java/com/ayanix/panther/impl/common/utils/MathsUtils.java
@@ -0,0 +1,56 @@
+/*
+ *                                                            _...---.._
+ *                                                        _.'`       -_ ``.
+ *                                                    .-'`                 `.
+ *                                                 .-`                     q ;
+ *                                              _-`                       __  \
+ *                                          .-'`                  . ' .   \ `;/
+ *                                      _.-`                    /.      `._`/
+ *                              _...--'`                        \_`..._
+ *                           .'`                         -         `'--:._
+ *                        .-`                           \                  `-.
+ *                       .                               `-..__.....----...., `.
+ *                      '                   `'''---..-''`'              : :  : :
+ *                    .` -                '``                           `'   `'
+ *                 .-` .` '             .``
+ *             _.-` .-`   '            .
+ *         _.-` _.-`    .' '         .`
+ * (`''--'' _.-`      .'  '        .'
+ *  `'----''        .'  .`       .`
+ *                .'  .'     .-'`    _____               _    _
+ *              .'   :    .-`       |  __ \             | |  | |
+ *              `. .`   ,`          | |__) |__ _  _ __  | |_ | |__    ___  _ __
+ *               .'   .'            |  ___// _` || '_ \ | __|| '_ \  / _ \| '__|
+ *              '   .`              | |   | (_| || | | || |_ | | | ||  __/| |
+ *             '  .`                |_|    \__,_||_| |_| \__||_| |_| \___||_|
+ *             `  '.
+ *             `.___;
+ */
+package com.ayanix.panther.impl.common.utils;
+
+import com.ayanix.panther.utils.common.IMathsUtils;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * Panther - Developed by Lewes D. B.
+ * All rights reserved 2017.
+ */
+public class MathsUtils implements IMathsUtils
+{
+
+	private final NumberFormat format;
+
+	public MathsUtils()
+	{
+		this.format = NumberFormat.getNumberInstance(Locale.US);
+	}
+
+	@Override
+	public String format(double value)
+	{
+		return format.format(value);
+	}
+
+}
