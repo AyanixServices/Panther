3cf4ed208e575cc5ef156f41eb2d2a8967a9b969
Rory Skipper <roryskipper123@gmail.com>
2021-08-17 10:47:41 -0600

    Added methods to compare a current plugin's version with the one required

--- Bukkit/src/main/java/com/ayanix/panther/impl/bukkit/utils/BukkitDependencyChecks.java
+++ Bukkit/src/main/java/com/ayanix/panther/impl/bukkit/utils/BukkitDependencyChecks.java
@@ -33,6 +33,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
@@ -107,6 +109,146 @@ public class BukkitDependencyChecks implements DependencyChecks
 		final Plugin dependency = Bukkit.getPluginManager().getPlugin(plugin);
 
 		return dependency != null && dependency.getDescription().getVersion().startsWith(version);
+	}
+
+	private List<Integer> getVersionValues(String version)
+	{
+		List<Integer> versionValues = new ArrayList<>();
+
+		while (!version.isEmpty())
+		{
+			try
+			{
+				int index = version.indexOf(".");
+
+				versionValues.add(Integer.parseInt(version.substring(0, index == -1 ? version.length() : index)));
+
+				if (index != -1)
+					version = version.substring(index + 1);
+				else
+					return versionValues;
+
+			} catch (IllegalArgumentException e)
+			{
+				return versionValues;
+			}
+		}
+
+		return versionValues;
+	}
+
+	public boolean isVersionAtLeast(final String plugin, final String version)
+	{
+		final Plugin dependency = Bukkit.getPluginManager().getPlugin(plugin);
+
+		if (dependency == null)
+			return false;
+
+		List<Integer> desiredVersionValues = getVersionValues(version);
+
+		String        pluginVersion = dependency.getDescription().getVersion();
+		List<Integer> versionValues = getVersionValues(pluginVersion);
+
+		for (int i = 0; i < desiredVersionValues.size(); i++)
+		{
+			int desiredNum = desiredVersionValues.get(i);
+			int realNum    = versionValues.size() > i ? versionValues.get(i) : 0;
+
+			if (desiredNum > realNum)
+				return false;
+			else if (desiredNum < realNum)
+				return true;
+		}
+
+		return true;
+	}
+
+	public boolean isVersionNoHigherThan(final String plugin, final String version)
+	{
+		final Plugin dependency = Bukkit.getPluginManager().getPlugin(plugin);
+
+		if (dependency == null)
+			return false;
+
+		List<Integer> desiredVersionValues = getVersionValues(version);
+
+		String        pluginVersion = dependency.getDescription().getVersion();
+		List<Integer> versionValues = getVersionValues(pluginVersion);
+
+		for (int i = 0; i < desiredVersionValues.size(); i++)
+		{
+			int desiredNum = desiredVersionValues.get(i);
+			int realNum    = versionValues.size() > i ? versionValues.get(i) : 0;
+
+			if (desiredNum < realNum)
+				return false;
+			else if (desiredNum > realNum)
+				return true;
+		}
+
+		return true;
+	}
+
+	public boolean isVersionHigherThan(final String plugin, final String version)
+	{
+		final Plugin dependency = Bukkit.getPluginManager().getPlugin(plugin);
+
+		if (dependency == null)
+			return false;
+
+		String pluginVersion = dependency.getDescription().getVersion();
+
+		// Faster to do this check first before parsing and comparing each value individually
+		if (version.equals(pluginVersion)) return false;
+
+		List<Integer> desiredVersionValues = getVersionValues(version);
+
+		List<Integer> versionValues = getVersionValues(pluginVersion);
+
+		for (int i = 0; i < Math.max(desiredVersionValues.size(), versionValues.size()); i++)
+		{
+			int desiredNum = desiredVersionValues.size() > i ? desiredVersionValues.get(i) : 0;
+			int realNum    = versionValues.size() > i ? versionValues.get(i) : 0;
+
+			if (desiredNum > realNum)
+				return false;
+			else if (desiredNum < realNum)
+				return true;
+		}
+
+		// If it reaches this point it means that all values were equal (version is the same)
+		return false;
+	}
+
+	public boolean isVersionLowerThan(final String plugin, final String version)
+	{
+		final Plugin dependency = Bukkit.getPluginManager().getPlugin(plugin);
+
+		if (dependency == null)
+			return false;
+
+		String pluginVersion = dependency.getDescription().getVersion();
+
+		// Faster to do this check first before parsing and comparing each value individually
+		if (version.equals(pluginVersion)) return false;
+
+		List<Integer> desiredVersionValues = getVersionValues(version);
+
+		List<Integer> versionValues = getVersionValues(pluginVersion);
+
+		for (int i = 0; i < Math.max(desiredVersionValues.size(), versionValues.size()); i++)
+		{
+			int desiredNum = desiredVersionValues.size() > i ? desiredVersionValues.get(i) : 0;
+			int realNum    = versionValues.size() > i ? versionValues.get(i) : 0;
+
+			if (desiredNum < realNum)
+				return false;
+			else if (desiredNum > realNum)
+				return true;
+		}
+
+		// If it reaches this point it means that all values were equal (version is the same)
+		return false;
 	}
 
 }
